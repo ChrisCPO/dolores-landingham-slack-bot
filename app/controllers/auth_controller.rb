7490e81a8169b47482e031412e4c6a3399666d06
ChrisCPO <ChrisCPO@gmail.com>
2016-08-28 17:09:21 -0400

    this address issue #170
    * allows a local developer to log-in;
    * bypasses oauth
    * see .sample.env
    * set developer email
    * set skip_team_check to 'true'

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,15 +2,35 @@ class AuthController < ApplicationController
   skip_before_action :authenticate_user!, only: [:oauth_callback]
 
   def oauth_callback
-    if team_member?
-      user = User.find_or_create_by(email: auth_email)
-      sign_in(user)
-      flash[:success] = "You successfully signed in"
-      redirect_to root_path
+    if ok_to_bypass_authentication?
+      create_user(developer_email)
+    else
+      if team_member?
+        create_user(auth_email)
+      end
     end
   end
 
   private
+
+  def create_user(email)
+    user = User.find_or_create_by(email: email)
+    sign_in(user)
+    flash[:success] = "You successfully signed in"
+    redirect_to root_path
+  end
+
+  def ok_to_bypass_authentication?
+    Rails.env == "development" && skip_team_check?
+  end
+
+  def skip_team_check?
+    ENV["SKIP_TEAM_CHECK"].downcase == "true"
+  end
+
+  def developer_email
+    ENV["DEVELOPER_EMAIL"]
+  end
 
   def team_member?
     auth_hash.credentials.team_member?
